1cedd6cb5ed89031bd75948afa27a7270127b6c8
ka-dyn <93449822+ka-dyn@users.noreply.github.com>
2021-11-05 09:48:46 -1000

    spelling error

--- kadynsWOTRMods/Tweaks/Deities/AllowForbiddenDeities.cs
+++ kadynsWOTRMods/Tweaks/Deities/AllowForbiddenDeities.cs
@@ -47,7 +47,7 @@ namespace kadynsWOTRMods.Tweaks.Deities {
                     BlueprintFeature GreenFaithFeature = Resources.GetBlueprint<BlueprintFeature>("99a7a8f13c1300c42878558fa9471e2f");
                     GreenFaithFeature.SetDescription("The Green Faith is a naturalistic philosophy based on the belief that natural forces are worthy of attention and respect. Followers of the Green Faith meditate daily, " +
                         "commune with natural forms of power, and show respect to nature in all things.  Sarkoris, prior to its destruction at the hands of demons, was formerly the Green Faith's greatest bastion; " +
-                        "since then, Green Faith holdouts continue their fight to protect nature from the corruption of the Worldwound. \nDomains: Air, Earth, Animal, Fire, Plant. \nFavored Weapons: Sickle, Quarterstaff");
+                        "since then, Green Faith holdouts continue their fight to protect nature from the corruption of the Worldwound. \nDomains: Air, Earth, Animal, Fire, Plant. \nFavoured Weapons: Sickle, Quarterstaff");
                     GreenFaithFeature.m_Icon = GreenFaithIcon;
                     GreenFaithFeature.RemoveComponents<PrerequisiteNoFeature>();
 
