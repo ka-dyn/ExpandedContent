37d80bf930c0a71a06397f6a992a4bdd52cda8b3
bittercranberry <theginge1@hotmail.com>
2022-07-30 10:08:40 +0100

    Fixed Blue Drake being incorrectly Cold Subtype

--- ExpandedContent/Tweaks/Classes/DrakeClass/DrakeCompanionBlue.cs
+++ ExpandedContent/Tweaks/Classes/DrakeClass/DrakeCompanionBlue.cs
@@ -26,7 +26,7 @@ namespace ExpandedContent.Tweaks.Classes.DrakeClass {
 
             var AnimalCompanionRank = Resources.GetBlueprint<BlueprintFeature>("1670990255e4fe948a863bafd5dbda5d");
             var DrakeCompanionClass = Resources.GetModBlueprint<BlueprintCharacterClass>("DrakeCompanionClass");
-            var DrakeSubtypeCold = Resources.GetModBlueprint<BlueprintFeature>("DrakeSubtypeCold");
+            var DrakeSubtypeAir = Resources.GetModBlueprint<BlueprintFeature>("DrakeSubtypeAir");
             var FormOfTheDragonBlueBuff = Resources.GetBlueprint<BlueprintBuff>("b117bc8b41735924dba3fb23318f39ff").GetComponent<Polymorph>();
             var BloodlineDraconicBlueProgression = Resources.GetBlueprint<BlueprintProgression>("8a7f100c02d0b254d8f5f3affc8ef386");
             var BlackDragon_Barks = Resources.GetBlueprint<BlueprintUnitAsksList>("3c0924a80e504f04c94de6ec2a28f9aa");
@@ -119,7 +119,7 @@ namespace ExpandedContent.Tweaks.Classes.DrakeClass {
                 bp.MaxHP = 0;
                 bp.m_AddFacts = new BlueprintUnitFactReference[] {
                     DrakeCompanionSlotFeature.ToReference<BlueprintUnitFactReference>(),
-                    DrakeSubtypeCold.ToReference<BlueprintUnitFactReference>(),
+                    DrakeSubtypeAir.ToReference<BlueprintUnitFactReference>(),
                     DragonType.ToReference<BlueprintUnitFactReference>(),
                     DrakeBloodBlue.ToReference<BlueprintUnitFactReference>(),
                     DrakeSizeTiny.ToReference<BlueprintUnitFactReference>()
