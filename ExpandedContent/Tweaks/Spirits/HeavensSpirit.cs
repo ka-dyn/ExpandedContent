ba1ef1db652de564521cb68a6d945218032e0033
bittercranberry <theginge1@hotmail.com>
2023-04-02 20:43:11 +0100

    Start of Heavens Spirit

---
+++ ExpandedContent/Tweaks/Spirits/HeavensSpirit.cs
@@ -0,0 +1,157 @@
+﻿using ExpandedContent.Extensions;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.Classes.Prerequisites;
+using Kingmaker.Blueprints;
+using Kingmaker.UnitLogic.FactLogic;
+using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.UnitLogic.Abilities.Blueprints;
+using Kingmaker.Designers.Mechanics.Facts;
+using Kingmaker.EntitySystem.Stats;
+using Kingmaker.UnitLogic.ActivatableAbilities;
+using Kingmaker.UnitLogic.Buffs.Blueprints;
+using Kingmaker.UnitLogic.Commands.Base;
+using Kingmaker.UnitLogic.Buffs.Components;
+using Kingmaker.UnitLogic.Abilities.Components.AreaEffects;
+using Kingmaker.ElementsSystem;
+using Kingmaker.Designers.EventConditionActionSystem.Actions;
+using Kingmaker.UnitLogic.Mechanics.Conditions;
+using Kingmaker.UnitLogic.Mechanics.Actions;
+using Kingmaker.UnitLogic;
+using Kingmaker.UnitLogic.Mechanics;
+using Kingmaker.RuleSystem;
+using Kingmaker.Utility;
+using Kingmaker.UnitLogic.Abilities.Components;
+using Kingmaker.Enums;
+using Kingmaker.UnitLogic.Abilities;
+using Kingmaker.UnitLogic.Mechanics.Properties;
+using Kingmaker.UnitLogic.Mechanics.Components;
+using Kingmaker.Visual.Animation.Kingmaker.Actions;
+using Kingmaker.Blueprints.Items.Ecnchantments;
+using Kingmaker.RuleSystem.Rules.Damage;
+using ExpandedContent.Config;
+using Kingmaker.Blueprints.Classes.Selection;
+using ExpandedContent.Utilities;
+
+namespace ExpandedContent.Tweaks.Spirits {
+    internal class HeavensSpirit {
+        public static void AddHeavensSprit() {
+
+            var ShamanClass = Resources.GetBlueprint<BlueprintCharacterClass>("145f1d3d360a7ad48bd95d392c81b38e");
+            var UnswornShamanArchetype = Resources.GetBlueprint<BlueprintArchetype>("556590a43467a27459ac1a80324c9f9f");
+            var ShamanHexSelection = Resources.GetBlueprint<BlueprintFeatureSelection>("4223fe18c75d4d14787af196a04e14e7");
+
+
+
+
+
+            //Spelllist
+            var ColorSpraySpell = Resources.GetBlueprintReference<BlueprintAbilityReference>("91da41b9793a4624797921f221db653c");
+            var RainbowPatternSpell = Resources.GetBlueprintReference<BlueprintAbilityReference>("4b8265132f9c8174f87ce7fa6d0fe47b");
+            var PrismaticSpraySpell = Resources.GetBlueprintReference<BlueprintAbilityReference>("b22fd434bdb60fb4ba1068206402c4cf");
+            var ChainLightningSpell = Resources.GetBlueprintReference<BlueprintAbilityReference>("645558d63604747428d55f0dd3a4cb58");
+            var SearingLightSpell = Resources.GetBlueprintReference<BlueprintAbilityReference>("bf0accce250381a44b857d4af6c8e10d");
+            var HypnoticPatternAbility = Resources.GetModBlueprint<BlueprintAbility>("HypnoticPatternAbility");
+            var SunburstSpell = Resources.GetBlueprintReference<BlueprintAbilityReference>("e96424f70ff884947b06f41a765b7658");
+            var PolarMidnightSpell = Resources.GetBlueprintReference<BlueprintAbilityReference>("ba48abb52b142164eba309fd09898856");
+            var BreakEnchantmentSpell = Resources.GetBlueprintReference<BlueprintAbilityReference>("7792da00c85b9e042a0fdfc2b66ec9a8");
+            var HeavensSpritSpells = Helpers.CreateBlueprint<BlueprintFeature>("HeavensSpritSpells", bp => {
+                bp.HideInCharacterSheetAndLevelUp = true;
+                bp.IsClassFeature = true;
+                bp.AddComponent<AddKnownSpell>(c => {
+                    c.m_CharacterClass = ShamanClass.ToReference<BlueprintCharacterClassReference>();
+                    c.m_Spell = ColorSpraySpell;
+                    c.SpellLevel = 1;
+                });
+                bp.AddComponent<AddKnownSpell>(c => {
+                    c.m_CharacterClass = ShamanClass.ToReference<BlueprintCharacterClassReference>();
+                    c.m_Spell = HypnoticPatternAbility.ToReference<BlueprintAbilityReference>();
+                    c.SpellLevel = 2;
+                });
+                bp.AddComponent<AddKnownSpell>(c => {
+                    c.m_CharacterClass = ShamanClass.ToReference<BlueprintCharacterClassReference>();
+                    c.m_Spell = SearingLightSpell;
+                    c.SpellLevel = 3;
+                });
+                bp.AddComponent<AddKnownSpell>(c => {
+                    c.m_CharacterClass = ShamanClass.ToReference<BlueprintCharacterClassReference>();
+                    c.m_Spell = RainbowPatternSpell;
+                    c.SpellLevel = 4;
+                });
+                bp.AddComponent<AddKnownSpell>(c => {
+                    c.m_CharacterClass = ShamanClass.ToReference<BlueprintCharacterClassReference>();
+                    c.m_Spell = BreakEnchantmentSpell;
+                    c.SpellLevel = 5;
+                });
+                bp.AddComponent<AddKnownSpell>(c => {
+                    c.m_CharacterClass = ShamanClass.ToReference<BlueprintCharacterClassReference>();
+                    c.m_Spell = ChainLightningSpell;
+                    c.SpellLevel = 6;
+                });
+                bp.AddComponent<AddKnownSpell>(c => {
+                    c.m_CharacterClass = ShamanClass.ToReference<BlueprintCharacterClassReference>();
+                    c.m_Spell = PrismaticSpraySpell;
+                    c.SpellLevel = 7;
+                });
+                bp.AddComponent<AddKnownSpell>(c => {
+                    c.m_CharacterClass = ShamanClass.ToReference<BlueprintCharacterClassReference>();
+                    c.m_Spell = SunburstSpell;
+                    c.SpellLevel = 8;
+                });
+                bp.AddComponent<AddKnownSpell>(c => {
+                    c.m_CharacterClass = ShamanClass.ToReference<BlueprintCharacterClassReference>();
+                    c.m_Spell = PolarMidnightSpell;
+                    c.SpellLevel = 9;
+                });
+            });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            var ShamanHeavensSpritProgression = Helpers.CreateBlueprint<BlueprintProgression>("ShamanHeavensSpritProgression", bp => {
+                bp.SetName("Heavens");
+                bp.SetDescription("A shaman who selects the heavens spirit has eyes that sparkle like starlight, exuding an aura of otherworldliness to those she is around. " +
+                    "When she calls upon one of this spirit’s abilities, her eyes turn pitch black and the colors around her drain for a brief moment.");
+                bp.AddComponent<AddFeaturesFromSelectionToDescription>(c => {
+                    c.Introduction = Helpers.CreateString("ShamanHeavensSpritProgression.AddFeaturesFromSelectionToDescription.Introduction", "Additional Hexes:");
+                    c.m_FeatureSelection = ShamanHexSelection.ToReference<BlueprintFeatureSelectionReference>();
+                    c.OnlyIfRequiresThisFeature = true;
+                });
+                bp.AddComponent<AddSpellsToDescription>(c => {
+                    c.Introduction = Helpers.CreateString("ShamanHeavensSpritProgression.AddSpellsToDescription.Introduction", "Bonus Spells:");
+                    c.m_SpellLists = new BlueprintSpellListReference[] { HeavensSpritSpells.ToReference<BlueprintSpellListReference>() };
+                });
+                bp.m_AllowNonContextActions = false;                
+                bp.Groups = new FeatureGroup[] { FeatureGroup.ShamanSpirit };
+                bp.IsClassFeature = true;
+                bp.m_Classes = new BlueprintProgression.ClassWithLevel[] {
+                    new BlueprintProgression.ClassWithLevel {
+                        m_Class = ShamanClass.ToReference<BlueprintCharacterClassReference>(),
+                        AdditionalLevel = 0
+                    }
+                };
+                bp.m_Archetypes = new BlueprintProgression.ArchetypeWithLevel[] {};
+                bp.LevelEntries = new LevelEntry[] {
+                    Helpers.LevelEntry(1, ShamanHeavensSpiritBaseFeature, HeavensSpritSpells),
+                    Helpers.LevelEntry(8, ShamanHeavensSpiritGreaterFeature),
+                    Helpers.LevelEntry(16, ShamanHeavensSpiritTrueFeature)
+                };
+                bp.GiveFeaturesForPreviousLevels = true;
+            });
+
+
+
+        }
+    }
+}
