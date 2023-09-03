37dea08f4c27f77b7820612bbc0f78eeea52cc69
bittercranberry <theginge1@hotmail.com>
2023-09-04 00:46:54 +0100

    Azathoth wrong weapon prof

--- ExpandedContent/Tweaks/TheElderMythos/Azathoth.cs
+++ ExpandedContent/Tweaks/TheElderMythos/Azathoth.cs
@@ -39,7 +39,7 @@ namespace ExpandedContent.Tweaks.TheElderMythos {
             BlueprintArchetype SwornOfTheEldestArchetype = Resources.GetModBlueprint<BlueprintArchetype>("SwornOfTheEldestArchetype");
 
 
-            BlueprintFeature WarhammerProficiency = Resources.GetBlueprint<BlueprintFeature>("aed4f88b52ae0fb468895f90da854ad4");
+            BlueprintFeature WarhammerProficiency = Resources.GetBlueprint<BlueprintFeature>("aba1be1d113ea4049b99ea92165e91dc");
             var AzathothIcon = AssetLoader.LoadInternal("Deities", "Icon_Azathoth.jpg");
             var AzathothFeature = Helpers.CreateBlueprint<BlueprintFeature>("AzathothFeature", (bp => {
 
