881550af31994ec5941b7e60f9646a4d293eaa05
bittercranberry <theginge1@hotmail.com>
2023-09-21 23:02:55 +0100

    Created Blessing tooling

---
+++ ExpandedContent/Tweaks/Blessings/WarBlessing.cs
@@ -0,0 +1,45 @@
+﻿using ExpandedContent.Extensions;
+using ExpandedContent.Utilities;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.Classes.Prerequisites;
+using Kingmaker.Designers.Mechanics.Facts;
+using Kingmaker.UnitLogic.FactLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpandedContent.Tweaks.Blessings {
+    internal class WarBlessing {
+        public static void AddWarBlessing() {
+
+            var WarDomainAllowed = Resources.GetBlueprintReference<BlueprintFeatureReference>("3795653d6d3b291418164b27be88cb43");
+            var WarpriestClass = Resources.GetBlueprintReference<BlueprintCharacterClassReference>("30b5e47d47a0e37438cc5a80c96cfb99");
+
+
+
+
+            var WarBlessingFeature = Helpers.CreateBlueprint<BlueprintFeature>("WarBlessingFeature", bp => {
+                bp.SetName("War");
+                bp.SetDescription("");
+                bp.AddComponent<AddFacts>(c => {
+                    c.m_Facts = new BlueprintUnitFactReference[] { WarBlessingMinorAbility.ToReference<BlueprintUnitFactReference>() };
+                });
+                bp.AddComponent<AddFeatureOnClassLevel>(c => {
+                    c.m_Class = WarpriestClass;
+                    c.Level = 10;
+                    c.m_Feature = WarBlessingMajorFeature.ToReference<BlueprintFeatureReference>();
+                    c.BeforeThisLevel = false;
+                });
+                bp.AddComponent<PrerequisiteFeature>(c => {
+                    c.CheckInProgression = true;
+                    c.HideInUI = true;
+                    c.m_Feature = WarDomainAllowed;
+                });
+                bp.Groups = new FeatureGroup[] { FeatureGroup.WarpriestBlessing };
+            });
+        }
+    }
+}
---
+++ ExpandedContent/Utilities/BlessingTools.cs
@@ -0,0 +1,50 @@
+﻿using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.Classes.Selection;
+using ExpandedContent.Extensions;
+using Kingmaker.Blueprints.Classes.Prerequisites;
+using Kingmaker.Designers.Mechanics.Facts;
+
+
+namespace ExpandedContent.Utilities {
+    public static class BlessingTools {
+        public static void RegisterBlessing(BlueprintProgression blessing) {
+            BlueprintFeatureSelection BlessingSelection = Resources.GetBlueprint<BlueprintFeatureSelection>("6d9dcc2a59210a14891aeedb09d406aa");
+            BlueprintFeatureSelection SecondBlessingSelection = Resources.GetBlueprint<BlueprintFeatureSelection>("b7ce4a67287cda746a59b31c042305cf");
+            BlessingSelection.m_AllFeatures = BlessingSelection.m_AllFeatures.AddToArray(blessing.ToReference<BlueprintFeatureReference>());
+            SecondBlessingSelection.m_AllFeatures = SecondBlessingSelection.m_AllFeatures.AddToArray(blessing.ToReference<BlueprintFeatureReference>());
+        }
+
+        public static void CreateDivineTrackerBlessing(string blueprintoutputname, BlueprintFeature warpriestblessing, string description) {
+
+            var RangerClass = Resources.GetBlueprintReference<BlueprintCharacterClassReference>("cda0615668a6df14eb36ba19ee881af6");
+            var DivineTrackerBlessingSelectionFirst = Resources.GetModBlueprint<BlueprintFeatureSelection>("DivineTrackerBlessingSelectionFirst");
+            var DivineTrackerBlessingSelectionSecond = Resources.GetModBlueprint<BlueprintFeatureSelection>("DivineTrackerBlessingSelectionSecond");
+
+            var blueprintfeature = Helpers.CreateBlueprint<BlueprintFeature>($"{blueprintoutputname}", bp => {
+                bp.m_DisplayName = warpriestblessing.m_DisplayName;
+                bp.SetDescription(description);
+                bp.Components = warpriestblessing.Components;
+                bp.IsClassFeature = false;
+                bp.AddComponent<PrerequisiteNoFeature>(c => {
+                    c.HideInUI = true;
+                    c.m_Feature = warpriestblessing.ToReference<BlueprintFeatureReference>();
+                });
+            });
+
+            var AddFeatureOnClassLevel = blueprintfeature.GetComponent<AddFeatureOnClassLevel>();
+            AddFeatureOnClassLevel.m_Class = RangerClass;
+            AddFeatureOnClassLevel.Level = 13;
+
+            warpriestblessing.AddComponent<PrerequisiteNoFeature>(c => {
+                c.HideInUI = true;
+                c.m_Feature = blueprintfeature.ToReference<BlueprintFeatureReference>();
+            });
+
+            DivineTrackerBlessingSelectionFirst.m_AllFeatures = DivineTrackerBlessingSelectionFirst.m_AllFeatures.AppendToArray(blueprintfeature.ToReference<BlueprintFeatureReference>());
+            DivineTrackerBlessingSelectionFirst.m_Features = DivineTrackerBlessingSelectionFirst.m_Features.AppendToArray(blueprintfeature.ToReference<BlueprintFeatureReference>());
+            DivineTrackerBlessingSelectionSecond.m_AllFeatures = DivineTrackerBlessingSelectionSecond.m_AllFeatures.AppendToArray(blueprintfeature.ToReference<BlueprintFeatureReference>());
+            DivineTrackerBlessingSelectionSecond.m_Features = DivineTrackerBlessingSelectionSecond.m_Features.AppendToArray(blueprintfeature.ToReference<BlueprintFeatureReference>());
+        }
+    }
+}
