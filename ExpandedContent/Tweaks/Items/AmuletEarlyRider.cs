46b61861a7b87dd93075316b6605bc77b884e532
bittercranberry <theginge1@hotmail.com>
2024-04-04 21:51:08 +0100

    Early Rider Fix

--- ExpandedContent/Tweaks/Items/AmuletEarlyRider.cs
+++ ExpandedContent/Tweaks/Items/AmuletEarlyRider.cs
@@ -18,25 +18,26 @@ namespace ExpandedContent.Tweaks.Items {
         public static void AddAmuletEarlyRider() {
 
             var ReducePerson = Resources.GetBlueprint<BlueprintAbility>("4e0e9aba6447d514f88eff1464cc4763");
-            var RiverFoxPendantItem = Resources.GetBlueprint<BlueprintItemEquipmentNeck>("169fd99b68c1dd04692c2368a7b957b7");
+            var RiverFoxPendantIcon = AssetLoader.LoadInternal("Items", "Icon_RiverFoxsPendant.png");
+
+            var MetalShardItem = Resources.GetBlueprintReference<BlueprintItemReference>("e6820e62423d4c81a2ba20d236251b67");
             var Jeweler_Chapter3VendorTable = Resources.GetBlueprint<BlueprintSharedVendorTable>("9f959fcff8d929042b1be6311d209580");
-            var DLC3_Tier1VendorTableMagic = Resources.GetBlueprint<BlueprintSharedVendorTable>("fac2be1b911745a588b5afcc19c0e184");
-            var DLC3_Tier2VendorTableMagic = Resources.GetBlueprint<BlueprintSharedVendorTable>("6176737df85b41fea25e13b67fe557b8"); 
+            var DLC3_VendorTable_Equipment = Resources.GetBlueprint<BlueprintSharedVendorTable>("195579adaa20483ca3aad66bb2b06f8f");
 
             var AmuletEarlyRider = Helpers.CreateBlueprint<BlueprintItemEquipmentNeck>("AmuletEarlyRider", bp => {
                 bp.SetName("Early Rider");
-                bp.SetDescription("This amulet allows the wearer to cast Reduce Person as a 6thlevel wizard once pey day.");
+                bp.SetDescription("This amulet allows the wearer to cast Reduce Person as a 6th level wizard once pey day.");
                 bp.SetFlavorText("A amulet common to those who raise animals for riding, allowing them to start training early.");
-                bp.m_Icon = RiverFoxPendantItem.Icon;
+                bp.m_Icon = RiverFoxPendantIcon;
                 bp.m_Cost = 600;
                 bp.m_Weight = 0.1f;
                 bp.m_IsNotable = false;
                 bp.m_ForceStackable = false;
                 bp.m_Destructible = true;
-                bp.m_ShardItem = RiverFoxPendantItem.m_ShardItem;
+                bp.m_ShardItem = MetalShardItem;
                 bp.m_MiscellaneousType = BlueprintItem.MiscellaneousItemType.None;
-                bp.m_InventoryPutSound = RiverFoxPendantItem.InventoryPutSound;
-                bp.m_InventoryTakeSound = RiverFoxPendantItem.InventoryTakeSound;
+                bp.m_InventoryPutSound = "AmuletPut";
+                bp.m_InventoryTakeSound = "AmuletTake";
                 bp.NeedSkinningForCollect = false;
                 bp.TrashLootTypes = new TrashLootType[0];
                 bp.CR = 10;
@@ -52,31 +53,16 @@ namespace ExpandedContent.Tweaks.Items {
                 bp.m_EquipmentEntity = null;
                 bp.m_EquipmentEntityAlternatives = new KingmakerEquipmentEntityReference[0];
                 bp.m_ForcedRampColorPresetIndex = 0;
-                bp.m_InventoryEquipSound = RiverFoxPendantItem.m_InventoryEquipSound;
+                bp.m_Enchantments = new BlueprintEquipmentEnchantmentReference[0];
+                bp.m_InventoryEquipSound = "AmuletPut";
             });
 
             Jeweler_Chapter3VendorTable.AddComponent<LootItemsPackFixed>(c => {
-                c.m_Item = new LootItem() {
-                    m_Type = LootItemType.Item,
-                    m_Item = AmuletEarlyRider.ToReference<BlueprintItemReference>(),
-                    m_Loot = new BlueprintUnitLootReference()
-                };
+                c.m_Item = VenderTools.CreateLootItem(AmuletEarlyRider);
                 c.m_Count = 6;
             });
-            DLC3_Tier1VendorTableMagic.AddComponent<LootItemsPackFixed>(c => {
-                c.m_Item = new LootItem() {
-                    m_Type = LootItemType.Item,
-                    m_Item = AmuletEarlyRider.ToReference<BlueprintItemReference>(),
-                    m_Loot = new BlueprintUnitLootReference()
-                };
-                c.m_Count = 6;
-            });
-            DLC3_Tier2VendorTableMagic.AddComponent<LootItemsPackFixed>(c => {
-                c.m_Item = new LootItem() {
-                    m_Type = LootItemType.Item,
-                    m_Item = AmuletEarlyRider.ToReference<BlueprintItemReference>(),
-                    m_Loot = new BlueprintUnitLootReference()
-                };
+            DLC3_VendorTable_Equipment.AddComponent<LootItemsPackFixed>(c => {
+                c.m_Item = VenderTools.CreateLootItem(AmuletEarlyRider);
                 c.m_Count = 6;
             });
 
